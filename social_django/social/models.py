c673318d81fa5bcd01a5dfbde9d233ea266f7b28
Vassallo Nicolás <42422704+NikozV@users.noreply.github.com>
2022-04-02 19:07:16 -0300

    archivos estáticos e imagenes

--- social_django/social/models.py
+++ social_django/social/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
@@ -5,7 +6,8 @@ from django.utils import timezone
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    #imagen
+    image = models.ImageField(default='batman.png')
+    
     def __str__(self):
         return f'Perfil de {self.user.username}'
 
